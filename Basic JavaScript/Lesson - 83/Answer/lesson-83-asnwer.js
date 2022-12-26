79ea7aa591b7b427de9c617c451d357236f64bb4
nabily4e-dev <nbl.dev21@gmail.com>
2022-12-26 20:58:57 +0300

    Reaname, style, and restruct

---
+++ Basic JavaScript/Lesson - 83/Answer/lesson-83-asnwer.js
@@ -0,0 +1,10 @@
+const myDog = {
+    // Only change code below this line
+  
+    "name": "Lola",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Humans", "Other dogs"]
+  };
+  
+    // Only change code above this line
