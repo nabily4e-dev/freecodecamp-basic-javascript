e4e4291273da1fce214370b3ba2f129e87f6711a
nabily4e-dev <nbl.dev21@gmail.com>
2022-12-28 10:30:28 +0300

    Lesson 92 completed

---
+++ Basic JavaScript/Lesson - 92/manipulating-complex-objects.js
@@ -0,0 +1,13 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
