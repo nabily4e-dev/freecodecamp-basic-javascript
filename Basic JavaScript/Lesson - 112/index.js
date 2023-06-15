47c7d860d09529543515f213690309125ba4ad12
NabiL Ashbat <nbl.dev21@gmail.com>
2023-06-15 12:15:13 +0300

    lesson 112 complete

--- Basic JavaScript/Lesson - 112/index.js
+++ Basic JavaScript/Lesson - 112/index.js
@@ -1,11 +1,13 @@
-// Only change code below this line
-function countdown(n){
-  if (n < 1){
-    return [];
-  } else {
-    const arrayCountdown = countdown(n - 1)
-    arrayCountdown.unshift(n)
-    return arrayCountdown
-  }
-}
-// Only change code above this line
+// // Only change code below this line
+// function countdown(n){
+//   if (n < 1){
+//     return [];
+//   } else {
+//     const arrayCountdown = countdown(n - 1)
+//     arrayCountdown.unshift(n)
+//     return arrayCountdown
+//   }
+// }
+// // Only change code above this line
+
+return n < 1 ? [] : [n].concat(countdown(n - 1));
